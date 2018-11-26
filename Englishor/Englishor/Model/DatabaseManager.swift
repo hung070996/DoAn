b2db83c71f605f7f5810eedb51ecfc9afa4999ce
do.tien.hung <do.tien.hung@TienHung.local>
2018-11-26 15:24:37 +0700

    ban nhap doc

--- Englishor/Englishor/Model/DatabaseManager.swift
+++ Englishor/Englishor/Model/DatabaseManager.swift
@@ -16,19 +16,22 @@ class DatabaseManager {
         do {
 //            let path = Bundle.main.path(forResource: "DoAnDB", ofType: "db")!
 //            return try Connection(path)
-//            return try Connection("/Users/dohung/Desktop/untitled folder 3/DoAn/Englishor/Englishor/Supporting/DoAnDB.db")
+//            let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+//                .appendingPathComponent("DoAnDB.db")
+//            print(fileURL)
+            return try Connection("/Users/do.tien.hung/Desktop/DoAn/DoAn/Englishor/Englishor/Supporting/DoAnDB.db")
 //            let path = NSSearchPathForDirectoriesInDomains(
 //                .documentDirectory, .userDomainMask, true
 //                ).first!
-            var path = NSSearchPathForDirectoriesInDomains(
-                .applicationSupportDirectory, .userDomainMask, true
-                ).first! + Bundle.main.bundleIdentifier!
-            
-            // create parent directory iff it doesn’t exist
-            try FileManager.default.createDirectory(
-                atPath: path, withIntermediateDirectories: true, attributes: nil
-            )
-            return try Connection("\(path)/DoAnDB.db")
+//            let path = NSSearchPathForDirectoriesInDomains(
+//                .applicationSupportDirectory, .userDomainMask, true
+//                ).first! + Bundle.main.bundleIdentifier!
+//
+//            // create parent directory iff it doesn’t exist
+//            try FileManager.default.createDirectory(
+//                atPath: path, withIntermediateDirectories: true, attributes: nil
+//            )
+//            return try Connection("\(path)/DoAnDB.db")
         } catch {
             return nil
         }
